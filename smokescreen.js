27bd1e5396b54b3b5f0ab25dded5ca2cee8c6ab9
Paul Gallagher <email": null>
2017-04-06 14:32:21 -0400

    committing

--- smokescreen.js
+++ smokescreen.js
@@ -1,13 +1,16 @@
-var linkObjects = document.getElementsByTagName("a")
-var i;
-var urls =[]
-for (i = 0; i< linkObjects.length; i++){
-  urls.push(linkObjects[i].href)
-}
-var url = urls[Math.floor(Math.random()*urls.length)];
-var window2 = window
-function redirect() {
-  window2.location.replace(url)
+function crawl(){
+  var linkObjects = document.getElementsByTagName("a")
+  var i;
+  var urls =[]
+  for (i = 0; i< linkObjects.length; i++){
+    urls.push(linkObjects[i].href)
+  }
+
+  var url = urls[Math.floor(Math.random()*urls.length)];
+
+  var window2 = window
+  setTimeout(window2.location.replace(url), 5000)
 };
 
-setTimeout(redirect, 5000)
+
+crawl()
